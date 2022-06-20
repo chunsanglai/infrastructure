bec9fba0efd2199c6c0eefffd25bc27c788ffe40
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 13:57:16 +0200

    change to private_dns

--- modules/ec2/route53.tf
+++ modules/ec2/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "private_record" {
   count   = var.private_hosted_zone_id == "" ? 0 : 1
   zone_id = var.private_hosted_zone_id
-  name    = var.name
+  name    = module.ec2_instance.private_dns
   type    = "A"
   ttl     = "60"
 
