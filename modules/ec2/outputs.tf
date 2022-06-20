c09794edec971ce88d681aa39d7600e53695b79a
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 13:28:46 +0200

    removed outputs

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -2,6 +2,3 @@ output "instance_ip" {
   description = "EC2 private ip"
   value       = module.ec2_instance.private_ip
 }
-output "private_dns" {
-  value = var.private_hosted_zone_id != "" ? aws_route53_record.private_record[0].fqdn : null
-}
