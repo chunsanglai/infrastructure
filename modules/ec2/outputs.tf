26495aea671d5c8b244084983c385a729e1bbec5
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 12:56:02 +0200

    ec2

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,7 +1,7 @@
-# output "instance_ip" {
-#   description = "EC2 private ip"
-#   value       = module.ec2_instance.private_ip[0]
-# }
-# output "private_dns" {
-#   value = var.private_hosted_zone_id != "" ? aws_route53_record.private_std_record[0].fqdn : null
-# }
+output "instance_ip" {
+  description = "EC2 private ip"
+  value       = module.ec2_instance.private_ip
+}
+output "private_dns" {
+  value = var.private_hosted_zone_id != "" ? aws_route53_record.private_record[0].fqdn : null
+}
