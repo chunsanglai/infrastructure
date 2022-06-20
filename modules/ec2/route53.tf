c4d6df6c2e93b0d93d76e2598f7caf5f6f4d0ab7
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 13:02:05 +0200

    ec2

--- modules/ec2/route53.tf
+++ modules/ec2/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "private_record" {
-  count   = var.private_hosted_zone_id == "" ? 0 : 1
-  zone_id = var.private_hosted_zone_id
+  count   = (length(var.private_hosted_zone_id) > 0 ? 1 : 0)
+  zone_id = var.private_hosted_zone_id[count.index]
   name    = var.name
   type    = "A"
   ttl     = "60"
