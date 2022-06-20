9980a0077414f30462e92771e9aef3131179f672
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 15:26:09 +0200

    deploy userdata

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -1,26 +1,32 @@
 #! /bin/bash
 LogMessage "Running package update"
-apt update -y
+sudo apt update -y
 LogMessage "Running package upgrade"
-apt upgrade -y
+sudo apt upgrade -y
 
 # Configure Cloudwatch agent
 LogMessage "Configure Cloudwatch agent"
 LogMessage "Download Cloudwatch package"
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 LogMessage "Install Cloudwatch package"
-dpkg -i -E ./amazon-cloudwatch-agent.rpm
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+
+LogMessage "Configure SSM agent"
+LogMessage "Download SSM package"
+sudo wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
+
 LogMessage "Cloudwatch package deleted after install"
-rm amazon-cloudwatch-agent.rpm
+sudo rm amazon-cloudwatch-agent.deb
 
 # Use cloudwatch config from SSM
 LogMessage "Activate Cloudwatch using SSM"
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
 -a fetch-config \
 -m ec2 \
 -c ssm:${ssm_cloudwatch_config} -s
 
 # Install AWS CLI package
 LogMessage "Installing AWS CLI package"
-apt install awscli -y
+sudo apt install awscli -y
 
