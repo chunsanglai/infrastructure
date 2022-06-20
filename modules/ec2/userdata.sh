d0e0989c3e62761baaf26213329c4f9a88776b86
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 14:57:52 +0200

    fix userdata

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -1,29 +1,26 @@
 #! /bin/bash
 set -e
 
-# Ouput all log
-exec > >(tee /var/log/user-data.log|logger -t user-data-extra -s 2>/dev/console) 2>&1
-
 # Make sure we have all the latest updates when we launch this instance
-apt-get update -y
-apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # Configure Cloudwatch agent
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-rpm -U ./amazon-cloudwatch-agent.rpm
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 
 # Configure Systems Manager agent
 wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
-dpkg -i amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
 
 # Use cloudwatch config from SSM
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
 -a fetch-config \
 -m ec2 \
 -c ssm:${ssm_cloudwatch_config} -s
 
 # Install AWS CLI package
 LogMessage "Installing AWS CLI package"
-apt install awscli -y
+sudo apt install awscli -y
 
 echo 'Done initialization'
