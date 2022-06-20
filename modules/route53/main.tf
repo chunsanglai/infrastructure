34d7774cf7f7f25ec2123796d829c741d5e13c56
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 11:33:05 +0200

    r53internal

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -15,15 +15,13 @@ resource "aws_route53_zone" "private" {
   }
 
   name = var.private_zone_name
-  tags = var.tags
   lifecycle {
     ignore_changes = [vpc]
   }
 }
-
 # Private zone authorization
 resource "aws_route53_vpc_association_authorization" "authorization" {
   for_each = toset(var.authorized_vpcs)
   vpc_id   = each.key
-  zone_id  = aws_route53_zone.private_zone.id
+  zone_id  = aws_route53_zone.private.id
 }
