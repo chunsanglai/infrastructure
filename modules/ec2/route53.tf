ae4579fafcc4faf7b76ea10e72d89bc66d03f6ed
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 13:14:00 +0200

    ec2

--- modules/ec2/route53.tf
+++ modules/ec2/route53.tf
@@ -1,9 +1,8 @@
 resource "aws_route53_record" "private_record" {
-  count   = (length(var.private_hosted_zone_id) > 0 ? 1 : 0)
-  zone_id = var.private_hosted_zone_id[count.index]
+  zone_id = var.private_hosted_zone_id
   name    = var.name
   type    = "A"
   ttl     = "60"
 
-  records = module.ec2_instance.private_ip
+  records = [module.ec2_instance.private_ip]
 }
