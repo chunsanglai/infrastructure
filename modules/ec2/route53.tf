e33b61b0e35d85c3ed3961f7de9fc53a83914c95
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 11:16:05 +0200

    ec2 r53

--- modules/ec2/route53.tf
+++ modules/ec2/route53.tf
@@ -1,7 +1,7 @@
-# resource "aws_route53_record" "public-record" {
-#   zone_id = aws_route53_zone.public.zone_id
-#   name    = module.ec2_instance.id
-#   type    = "A"
-#   ttl     = "300"
-#   records = [aws_instance.ec2_instance.public_ip]
-# }
+resource "aws_route53_record" "public-record" {
+  zone_id = aws_route53_zone.public.zone_id
+  name    = module.ec2_instance.id
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.ec2_instance.public_ip]
+}
