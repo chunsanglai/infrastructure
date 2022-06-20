12f546fd680a62e6880ce4e931bd18f2ecea65f4
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 17:49:04 +0200

    deploy new ec2

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -6,8 +6,8 @@ output "private_dns" {
   value = var.private_hosted_zone_id != "" ? aws_route53_record.private_record[0].fqdn : null
 }
 output "security_group" {
-  value = aws_security_group.security_group.id
+  value = aws_security_group.security_group.id[count.index]
 }
 output "internal_security_group" {
-  value = aws_security_group.internal_security_group.id
+  value = aws_security_group.internal_security_group.id[count.index]
 }
