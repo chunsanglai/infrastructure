b71b8299bb5b98d754ac9c1b2fb895fda629618f
Chunsang Lai <chunsanglai@gmail.com>
2022-06-20 11:31:58 +0200

    r53internal

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "aws_route53_zone" {
-  source      = "./modules/route53"
-  public_zone_name = "chunsang.lai"
+  source            = "./modules/route53"
+  public_zone_name  = "chunsang.lai"
   private_zone_name = "chunsang.lai.internal"
   vpc_id            = module.vpc.vpc_id
 }
