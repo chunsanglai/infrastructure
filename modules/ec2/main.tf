632acbb18b0ad3e193fed4524f22008ff0516656
Chunsang Lai <chunsanglai@gmail.com>
2022-06-21 15:43:55 +0200

    sg

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -52,28 +52,26 @@ resource "aws_security_group" "public-security-group" {
   }
 }
 resource "aws_security_group" "ec2_security_groups" {
-  name   = "huwgqyeuqxh"
+  name        = join("-", [var.name, "iets-sg"])
   vpc_id = var.vpc_id
+  dynamic "ingress" {
+    for_each = var.sg_ingress_rules
+    content {
+      from_port = var.sg_ingress_rules[count.index].from_port
+      to_port = var.sg_ingress_rules[count.index].to_port
+      protocol = var.sg_ingress_rules[count.index].protocol
+      cidr_blocks = [var.sg_ingress_rules[count.index].cidr_block]
+      description       = var.sg_ingress_rules[count.index].description
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-resource "aws_security_group_rule" "ingress_rules" {
-  count = length(var.sg_ingress_rules)
 
-  type              = "ingress"
-  from_port         = var.sg_ingress_rules[count.index].from_port
-  to_port           = var.sg_ingress_rules[count.index].to_port
-  protocol          = var.sg_ingress_rules[count.index].protocol
-  cidr_blocks       = [var.sg_ingress_rules[count.index].cidr_block]
-  description       = var.sg_ingress_rules[count.index].description
-  security_group_id = aws_security_group.ec2_security_groups.id
-}
-resource "aws_security_group_rule" "egress_rules" {
-  type              = "egress"
-  to_port           = 0
-  from_port         = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ec2_security_groups.id
-}
 #Data Volume
 resource "aws_volume_attachment" "this" {
   device_name = "/dev/sdh"
