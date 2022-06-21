3ba8478083335ce6862629a2d4fe3b82fbae6f4c
Chunsang Lai <chunsanglai@gmail.com>
2022-06-21 14:48:11 +0200

    remove sns resources

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -2,13 +2,5 @@ resource "aws_sns_topic" "ok" {
   name              = var.ok
   kms_master_key_id = aws_kms_key.this.arn
 }
-resource "aws_sns_topic" "warning" {
-  name              = var.warning
-  kms_master_key_id = aws_kms_key.this.arn
-}
-resource "aws_sns_topic" "critical" {
-  name              = var.critical
-  kms_master_key_id = aws_kms_key.this.arn
-}
 resource "aws_kms_key" "this" {
 }
