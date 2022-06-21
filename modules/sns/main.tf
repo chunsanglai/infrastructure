a0fb115f36dbd24bef2748dc33aeed02e95705c6
Chunsang Lai <chunsanglai@gmail.com>
2022-06-21 14:49:19 +0200

    remove sns resources

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -1,5 +1,5 @@
-resource "aws_sns_topic" "ok" {
-  name              = var.ok
+resource "aws_sns_topic" "sns" {
+  name              = var.sns
   kms_master_key_id = aws_kms_key.this.arn
 }
 resource "aws_kms_key" "this" {
--- modules/sns/variables.tf
+++ modules/sns/variables.tf
@@ -2,16 +2,7 @@ variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "eu-central-1"
 }
-variable "ok"{
+variable "sns"{
   description = "sns name for ok warning"
-  default = "ok-topic"
-}
-variable "warning" {
-  description = "sns name for info warning"
-  default = "warning-topic"
-
-}
-variable "critical" {
-  description = "sns name for critical warning" 
-  default = "critical-topic"
+  default = "sns-topic"
 }
