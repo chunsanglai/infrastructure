fc0465b2d60ea48a11f0ea10c7064ee6872e7c95
Chunsang Lai <chunsanglai@gmail.com>
2022-06-21 16:06:52 +0200

    sg

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "public-security-group" {
 resource "aws_security_group" "ec2_security_groups" {
   name        = join("-", [var.name, "snap-niet-sg"])
   vpc_id = var.vpc_id
-  count       = length(var.sg_ingress_rules)
+  count = var.sg_ingress_rules
   dynamic "ingress" {
     for_each = length(var.sg_ingress_rules)
     content {
