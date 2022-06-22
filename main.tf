acc34411d45221d58cd2e964e3d0d267fb86e548
Chunsang Lai <chunsanglai@gmail.com>
2022-06-22 10:37:22 +0200

    deploy rds

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ module "rds" {
   source              = "./modules/rds"
   name                = "test-rds"
   engine              = "aurora-mysql"
-  engine_version      = "5.7.12"
+  engine_version      = "5.7.mysql_aurora.2.10.2"
   vpc_id              = module.vpc.vpc_id
   allowed_cidr_blocks = module.vpc.subnet_database_subnets_cidr_blocks
 }
