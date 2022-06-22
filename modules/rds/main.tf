328c6874c96ab5e3805940380e1ae3c3b7743894
Chunsang Lai <chunsanglai@gmail.com>
2022-06-22 13:07:57 +0200

    add db instance

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -4,12 +4,12 @@ resource "random_password" "master"{
   override_special = "_!%^"
 }
 
-data "aws_secretsmanager_secret" "password" {
+resource "aws_secretsmanager_secret" "password" {
   name = "${var.name}-db-password"
 }
 
-data "aws_secretsmanager_secret_version" "password" {
-  secret_id = data.aws_secretsmanager_secret.password
+resource "aws_secretsmanager_secret_version" "password" {
+  secret_id     = aws_secretsmanager_secret.password.id
   secret_string = random_password.master.result
 }
 
