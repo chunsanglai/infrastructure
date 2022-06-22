cf9f8b24cff90446ec45069846e7ee4e33b25bca
Chunsang Lai <chunsanglai@gmail.com>
2022-06-22 10:21:27 +0200

    remove ec2

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,13 +6,25 @@ output "subnet_public_subnet_ids" {
   description = "List of IDs of public subnets"
   value       = module.vpc.public_subnets
 }
+output "subnet_public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = module.vpc.public_subnets_cidr_blocks
+}
 output "subnet_private_subnet_ids" {
   description = "List of IDs of private subnets"
   value       = module.vpc.private_subnets
 }
+output "subnet_private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = module.vpc.private_subnets_cidr_blocks
+}
 output "subnet_database_subnet_ids" {
   description = "List of IDs of database subnets"
   value       = module.vpc.database_subnets
+}
+output "subnet_database_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of database subnets"
+  value       = module.vpc.database_subnets_cidr_blocks
 }
 output "azs" {
   value = module.vpc.azs
