884b25451d0c0ae0837a92b5a807acdd6d0b5ac7
Chunsang Lai <chunsanglai@gmail.com>
2022-06-22 15:23:06 +0200

    instances count

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -40,7 +40,7 @@ module "rds-aurora" {
   engine         = var.engine
   engine_version = var.engine_version
   instance_class = var.instance_class
-  count = length(var.db_instances)
+  count = var.db_instances
   instances = {var.db_instances[count.index] = {}}
   kms_key_id  = aws_kms_key.this.arn
   storage_encrypted = true
