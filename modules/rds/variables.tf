ec7c7045cdac508ee2ab2155f145530cad82c9e4
Chunsang Lai <chunsanglai@gmail.com>
2022-06-22 13:10:21 +0200

    add db instance

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -25,8 +25,3 @@ variable "database_subnet_group_name" {
 variable "instance_class" {
   description = "The instance type of the RDS instance."
 }
-output "db_instance_password" {
-  description = "The database password (this password may be old, because Terraform doesn't track it after initial creation)"
-  value       = module.db.db_instance_password
-  sensitive   = true
-}
