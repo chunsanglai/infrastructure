2345e2f9c5904b8a1f32a89448940425b42b3f2b
Chunsang Lai <chunsanglai@gmail.com>
2022-06-22 10:51:50 +0200

    deploy rds

--- main.tf
+++ main.tf
@@ -54,12 +54,12 @@ module "ec2" {
   internal_ingress_rules = []
 }
 
-# module "rds" {
-#   source                     = "./modules/rds"
-#   name                       = "test-rds"
-#   engine                     = "aurora-mysql"
-#   engine_version             = "5.7.mysql_aurora.2.10.2"
-#   vpc_id                     = module.vpc.vpc_id
-#   database_subnet_group_name = module.vpc.database_subnet_group_name
-#   allowed_cidr_blocks        = module.vpc.subnet_database_subnets_cidr_blocks
-# }
+module "rds" {
+  source                     = "./modules/rds"
+  name                       = "test-rds"
+  engine                     = "aurora-mysql"
+  engine_version             = "5.7.mysql_aurora.2.10.2"
+  vpc_id                     = module.vpc.vpc_id
+  database_subnet_group_name = module.vpc.database_subnet_group_name
+  allowed_cidr_blocks        = module.vpc.subnet_database_subnets_cidr_blocks
+}
