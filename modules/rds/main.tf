d7b3994274a3e985605628045b3bdd640e3e83e8
Chunsang Lai <chunsanglai@gmail.com>
2022-06-23 10:48:32 +0200

    deploy cw

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -83,7 +83,7 @@ resource "aws_rds_cluster_parameter_group" "rds_group" {
 }
 #Cloudwatch Alarm for Storage
 resource "aws_cloudwatch_metric_alarm" "storage-low-alarm" {
-  for_each = keys(var.instances)
+  for_each = var.instances
   alarm_name                = "${var.name}-${var.instances}-storage-low-alarm"
   alarm_description         = "This metric monitors database storage dipping below threshold"
   comparison_operator       = "LessThanThreshold"
