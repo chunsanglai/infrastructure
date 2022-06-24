943d2f70ed541c411d557f08a32de7725fdc5170
Chunsang Lai <chunsanglai@gmail.com>
2022-06-24 09:48:59 +0200

    deploy tags

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -6,6 +6,3 @@ output "instance_id" {
   description = "EC2 id"
   value       = module.ec2_instance.id
 }
-output "private_dns" {
-  value = var.private_hosted_zone_id != "" ? aws_route53_record.private_record[0].fqdn : null
-}
