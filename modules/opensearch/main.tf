5eb11209b7a9751dbafb7514d7c6cf99e4455bbc
Chunsang Lai <chunsanglai@gmail.com>
2022-06-27 09:12:13 +0200

    update repo

--- modules/opensearch/main.tf
+++ modules/opensearch/main.tf
@@ -1,4 +1,33 @@
-# OpenSearch domain
+#Create password
+resource "random_password" "password"{
+  length           = 16
+  special          = true
+  override_special = "_!%^"
+}
+
+resource "aws_secretsmanager_secret" "password" {
+  name = "${var.name}-opensearch-password"
+}
+
+resource "aws_secretsmanager_secret_version" "password" {
+  secret_id     = aws_secretsmanager_secret.password.id
+  secret_string = <<EOF
+   {
+    "username": "root",
+    "password": "${random_password.password.result}"
+   }
+EOF
+}
+#Import password
+data "aws_secretsmanager_secret" "password" {
+  arn = aws_secretsmanager_secret.password.arn
+}
+data "aws_secretsmanager_secret_version" "credentials" {
+  secret_id = data.aws_secretsmanager_secret.password.arn
+}
+locals {
+  creds = jsondecode(data.aws_secretsmanager_secret_version.credentials.secret_string)
+}# OpenSearch domain
 resource "aws_elasticsearch_domain" "opensearch" {
   domain_name           = var.domain
   elasticsearch_version = var.opensearch_version
@@ -16,8 +45,8 @@ resource "aws_elasticsearch_domain" "opensearch" {
     enabled                        = true
     internal_user_database_enabled = true
     master_user_options {
-      master_user_name     = var.user_name
-      master_user_password = var.password
+      master_user_name     = local.creds.username 
+      master_user_password = local.creds.password
     }
   }
   encrypt_at_rest {
