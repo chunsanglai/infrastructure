9f6d122b41679ae512bbb69d52f2e281b28b2ace
Chunsang Lai <chunsanglai@gmail.com>
2022-06-27 12:55:57 +0200

    add opensearch

--- main.tf
+++ main.tf
@@ -78,3 +78,20 @@ module "os" {
     Managedby    = "Terraform"
   }
 }
+module "os-1" {
+  source             = "./modules/opensearch"
+  vpc_id             = module.vpc.vpc_id
+  domain             = "os-chuns12345676"
+  opensearch_version = "OpenSearch_1.2"
+  instance_type      = "t3.small.elasticsearch"
+  subnet_ids         = [module.vpc.subnet_private_subnet_ids[0]]
+  allowed_security_groups = [] 
+  instance_count     = "1"
+  volume_size        = "10"
+  volume_type        = "gp2" #doesnt support GP3 yet
+  tags = {
+    CostCenter   = "chun"
+    map-migrated = "d-server-12345"
+    Managedby    = "Terraform"
+  }
+}
