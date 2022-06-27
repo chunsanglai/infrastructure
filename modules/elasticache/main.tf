90cbdbe1c9e6f00c11ab7ddd308ec5fbf9a827c0
Chunsang Lai <chunsanglai@gmail.com>
2022-06-27 14:59:26 +0200

    rm opensearch

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -1,19 +1,19 @@
 resource "aws_elasticache_cluster" "example" {
-  cluster_id           = "cluster-example"
+  cluster_id           = var.name
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis3.2"
   engine_version       = "3.2.10"
   port                 = 6379
-  log_delivery_configuration {
-    destination      = aws_cloudwatch_log_group.example.name
-    destination_type = "cloudwatch-logs"
-    log_format       = "text"
-    log_type         = "slow-log"
-  }
+  apply_immediately = "true"
+  auto_minor_version_upgrade = "true"
+#   log_delivery_configuration {
+#     destination      = aws_cloudwatch_log_group.example.name
+#     destination_type = "cloudwatch-logs"
+#     log_format       = "text"
+#     log_type         = "slow-log"
+#   }
+  subnet_group_name = var.subnet_ids
 }
 
-apply_immediately 
-auto_minor_version_upgrade 
-availability_zone 
---
+++ modules/elasticache/variables.tf
@@ -0,0 +1,8 @@
+variable "subnet_ids" {
+  type        = list(string)
+  description = "Subnet IDs."
+}
+variable "name" {
+    type = string
+    description = "(optional) describe your variable"
+}
