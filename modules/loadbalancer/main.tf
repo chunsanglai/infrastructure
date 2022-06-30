e296ed2f02c45f712a1746830df51b1642a47e14
Chunsang Lai <chunsanglai@gmail.com>
2022-06-30 09:42:17 +0200

    change alb

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -1,21 +1,33 @@
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 7.0"
-
-  name = var.name 
-
+resource "aws_lb" "test" {
+  name               = var.name
+  internal           = false
   load_balancer_type = var.load_balancer_type
-
-  vpc_id             = var.vpc_id
+  security_groups    = [module.security_group.id]
   subnets            = var.subnet_ids
-  target_groups = for_each (var.targets)
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+
+  enable_deletion_protection = true
+
+  # access_logs {
+  #   bucket  = aws_s3_bucket.lb_logs.bucket
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
+
+  # tags = {
+  #   Environment = "production"
+  # }
+}
+
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = var.target_id
+  port             = 80
 }
 
 module "security_group" {
