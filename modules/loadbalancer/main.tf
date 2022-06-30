90c9d923f05737facd9aab7b3dd890131d1fdc09
Chunsang Lai <chunsanglai@gmail.com>
2022-06-30 09:49:05 +0200

    change alb

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -35,7 +35,18 @@ resource "aws_security_group" "allow_lb" {
   description = "Security group for example usage with ALB"
   vpc_id      = var.vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
-  egress_rules        = ["all-all"]
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
