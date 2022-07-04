644be820a125a04fb75dedac55d5b1c97b866b09
Chunsang Lai <chunsanglai@gmail.com>
2022-07-04 10:17:09 +0200

    acm for domain

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -1,7 +1,11 @@
+locals {
+  subject_alternative_names = concat(["*.${var.domain_name}"], var.custom_sub_domain_names)
+}
+
 resource "aws_acm_certificate" "cert" {
-  domain_name       = "*.${var.domain_name}"
+  domain_name       = "${var.domain_name}"
   validation_method = "DNS"
-  subject_alternative_names = ["${var.domain_name}"]
+  subject_alternative_names = local.subject_alternative_names
 
   lifecycle {
     create_before_destroy = true
--- modules/acm/variables.tf
+++ modules/acm/variables.tf
@@ -6,3 +6,7 @@ variable "zone_id" {
     type = string
     description = "(optional) describe your variable"
 }
+variable "custom_sub_domain_names" {
+  type    = list(string)
+  default = []
+}
