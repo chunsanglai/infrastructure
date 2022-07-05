546660cb947d528d4cec535ff860166199302a19
Chunsang Lai <chunsanglai@gmail.com>
2022-07-05 11:04:32 +0200

    add ec2

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket                      = "terraform-state-chunsang"
     region                      = "eu-central-1"
-    dynamodb_table              = "terraform-state-lock"
+    dynamodb_table              = "terraform-lock-state"
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_credentials_validation = true
