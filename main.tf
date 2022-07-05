bea457ee2208fb4df42a686b8e291c8dbcc3f309
Chunsang Lai <chunsanglai@gmail.com>
2022-07-05 11:19:49 +0200

    vpc

--- main.tf
+++ main.tf
@@ -10,27 +10,27 @@ module "aws_route53_zone" {
 # module "sns" {
 #   source = "./modules/sns"
 # }
-# module "vpc" {
-#   source                  = "./modules/vpc"
-#   name                    = "chun"
-#   env                     = "test"
-#   aws_region              = "eu-central-1"
-#   azs                     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-#   cidr                    = "10.55.0.0/16"
-#   public_subnets          = ["10.55.1.0/24", "10.55.2.0/24", "10.55.3.0/24"]
-#   private_subnets         = ["10.55.4.0/24", "10.55.5.0/24", "10.55.6.0/24"]
-#   database_subnets        = ["10.55.7.0/24", "10.55.8.0/24", "10.55.9.0/24"]
-#   enable_nat_gateway      = false
-#   single_nat_gateway      = false
-#   deletion_window_in_days = 30
-#   log_destination_type    = "s3"
-#   traffic_type            = "ALL"
-#   tags = {
-#     CostCenter   = "chun1"
-#     map-migrated = "d-server-12345"
-#     Managedby    = "Terraform"
-#   }
-# }
+module "vpc" {
+  source                  = "./modules/vpc"
+  name                    = "chun"
+  env                     = "test"
+  aws_region              = "eu-central-1"
+  azs                     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  cidr                    = "10.55.0.0/16"
+  public_subnets          = ["10.55.1.0/24", "10.55.2.0/24", "10.55.3.0/24"]
+  private_subnets         = ["10.55.4.0/24", "10.55.5.0/24", "10.55.6.0/24"]
+  database_subnets        = ["10.55.7.0/24", "10.55.8.0/24", "10.55.9.0/24"]
+  enable_nat_gateway      = false
+  single_nat_gateway      = false
+  deletion_window_in_days = 30
+  log_destination_type    = "s3"
+  traffic_type            = "ALL"
+  tags = {
+    CostCenter   = "chun1"
+    map-migrated = "d-server-12345"
+    Managedby    = "Terraform"
+  }
+}
 
 # module "stg-alb" {
 #   source                     = "./modules/loadbalancer"
